c9e0e72cbaba984f6dcfc6708e51a8a726cc0b86
Mrinal Haque <mrinalhaque99@gmail.com>
2022-05-10 20:53:29 +0600

    shesvg init

---
+++ shesvg.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: SHESVG Elementor
+ * Description: Assessment of Pixelaar
+ * Author: Mrinal Haque
+ * Author URI: https://mrinal.com
+ * Plugin URI: https://github.com/mrinal013/shesvg
+ */
